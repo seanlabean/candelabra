e8167af92be2f8f02027e7fae7ee300ca7dc28cd
seanlabean <sean.phys@gmail.com>
2024-08-16 14:52:36 -0400

    add plotting

---
+++ frontend/src/components/Plot.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+function Plot({ data, xKey, yKey, title }) {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <ResponsiveContainer width="100%" height={300}>
+                <LineChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey={xKey} />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey={yKey} stroke="#8884d8" activeDot={{ r: 8}} />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    );
+}
+
+export default Plot;
