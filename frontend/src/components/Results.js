5aabd09281f1295df37d7e4d1c054f751eafa585
seanlabean <sean.phys@gmail.com>
2024-08-16 14:53:31 -0400

    add plots to results to display

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -4,7 +4,9 @@ function Results({ data }) {
     return (
         <div className="results">
             <h2>Stock Data</h2>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <Plot data={data} xKey="Date" yKey="Close" title="Closing Prices" />
+            <Plot data={data} xKey="Date" yKey="rolling_mean_5" title="5-Day Rolling Mean" />
+            <Plot data={data} xKey="Date" yKey="rolling_std_5" title="5-Day Rolling Std Dev" />
         </div>
     );
 }
