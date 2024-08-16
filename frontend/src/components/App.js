835ebd713ba8c386fb78b28d7fbf974cb4464b68
seanlabean <sean.phys@gmail.com>
2024-08-16 15:22:58 -0400

    ignore db and cleanup frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -19,25 +19,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="App">
         <h1>Stock Prediction App</h1>
         <StockInput onSubmit={fetchData} />
         {results && <Results data={results} />}
-      </div>
     </div>
   );
 }
