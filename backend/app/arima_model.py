a8a036ecc4c9b34a952e17b2fc0e49f3f32dabe7
seanlabean <sean.phys@gmail.com>
2024-08-19 13:12:46 -0400

    defining arima module

--- backend/app/arima_model.py
+++ backend/app/arima_model.py
@@ -8,3 +8,30 @@ def check_stationarity(timeseries):
     (non-stationary) (MacKinnon; 1994, 2010)."""
     result = adfuller(timeseries)
     return result[1] <= 0.05
+
+def difference_data(df, column):
+    df[f'{column}_diff'] = df[column].diff().dropna()
+    return df
+
+def train_arima_model(df, column, order):
+    model = ARIMA(df[column], order=order)
+    model_fit = model.fit()
+    return model_fit
+
+def hyperparameter_tuning(df, column):
+    p = d = q = range(0, 3)
+    pdq = [(x,y,z) for x in p for y in d for z in q]
+    best_aic = float("inf")
+    best_pdq = None
+    for param in pdq:
+        try:
+            model_fit = train_arima_model(df, column, param)
+            if model_fit.aic < best_aic:
+                best_aic = model_fit.aic
+                best_pdq = param
+        except:
+            continue
+    return best_pdq
+
+def make_prediction(model_fit, steps):
+    return model_fit.forecast(steps=steps)
